059442d3e13addf082a74262302e03364b07edc2
Dabigorna <dabigas3@gmail.com>
2026-06-23 19:49:17 -0300

    feat: Inicial sctructure of my project

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes, serialization
+
